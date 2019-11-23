cf7f3fa854f8afda23bea3d4ca0bec8fa59e7b2f
Andrey Kovalenko <ak@taucode.com>
2019-11-24 00:42:05 +0200

    Main functionality ready.

---
+++ tests/TauCode.WebApi.Host.Cqrs.Tests/DummyFixture.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace TauCode.WebApi.Host.Cqrs.Tests
+{
+    [TestFixture]
+    public class DummyFixture
+    {
+        [Test]
+        public void DummyTest()
+        {
+            Assert.Pass("Passed for Jenkins.");
+        }
+    }
+}
