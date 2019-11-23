2eff8693329c9ed9087e01b65ad277aeb5b8b6af
Andrey Kovalenko <ak@taucode.com>
2019-11-24 00:22:23 +0200

    Projects added.

---
+++ src/TauCode.WebApi.Host.Cqrs/WebApiHostCqrsExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TauCode.WebApi.Host.Cqrs
+{
+    public static class WebApiHostCqrsExtensions
+    {
+        public static IAppStartup AddCqrs(this IAppStartup startup)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
